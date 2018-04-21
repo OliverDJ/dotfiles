5ba06258e4d15fb7277930e25b09d6159f935e35
OliverDJ <oliver.damsgaard@gmail.com>
2018-04-21 16:38:01 +0200

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,14 +1,14 @@
 # GIT
-GIT_NAME="Håvard Falk"
-GIT_EMAIL="hfalk4@gmail.com"
+GIT_NAME="Oliver Damsgaard Jensen"
+GIT_EMAIL="oliver.damsgaard@gmail.com"
 
 # APPLICATIONS
 # The applications below are regular apps such as Chrome and Spotify.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="alfred android-studio appcleaner atom google-backup-and-sync basictex
-  calibre flux google-chrome intellij-idea iterm2 java jetbrains-toolbox postman
-  pycharm r-app skype skype-for-business slack spectacle spotify torbrowser
+BREW_CASKS="android-studio appcleaner atom google-backup-and-sync basictex
+  flux google-chrome intellij-idea iterm2 java jetbrains-toolbox postman
+  pycharm r-app skype slack spectacle spotify torbrowser
   transmission veracrypt vlc"
 
 # NODE
@@ -22,7 +22,7 @@ APM_PACKAGES="atom-beautify autoclose-html autocomplete-paths busy-signal color-
   pigments todo-show vim-mode"
 
 # BREW (OPTIONAL)
-OPTIONAL_BREW_PACKAGES="bazel gnupg nmap nvm pyenv wget yarn"
+OPTIONAL_BREW_PACKAGES="gnupg nmap nvm pyenv wget yarn"
 
 # --------------------------------------------------------------
 # DANGER ZONE! EDIT THE FOLLOWING SETTINGS WITH CARE
